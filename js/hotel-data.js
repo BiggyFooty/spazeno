22e1627c6f61e0ec608e3656b26e56bfbc8d00d1
randomguyforreal <hellking298@gmail.com>
2025-03-12 21:46:18 +0300

    v15

---
+++ js/hotel-data.js
@@ -0,0 +1,226 @@
+/**
+ * Sparez - Hotel Data
+ * This file contains the list of hotels available in the application
+ */
+
+// Define the hotel data
+var hotelData = [
+  {
+    "otel_isim": "Elite World",
+    "sube_adi": "Elite World Europe",
+    "adres": "Yeşilköy Mah., Havaalanı Caddesi No: 34, Bakırköy, İstanbul",
+    "google_yildiz": 4.3,
+    "fotograf_linki": "https://www.google.com/search?q=Elite+World+Europe+hotel"
+  },
+  {
+    "otel_isim": "Elite World",
+    "sube_adi": "Elite World Business",
+    "adres": "Kocatepe Mah., Abide-i Hürriyet Cd. No: 29, Şişli, İstanbul",
+    "google_yildiz": 4.4,
+    "fotograf_linki": "https://www.google.com/search?q=Elite+World+Business+hotel"
+  },
+  {
+    "otel_isim": "Radisson Blu",
+    "sube_adi": "Radisson Blu Bosphorus",
+    "adres": "Çırağan Caddesi No: 46, Beşiktaş, İstanbul",
+    "google_yildiz": 4.5,
+    "fotograf_linki": "https://www.google.com/search?q=Radisson+Blu+Bosphorus+hotel"
+  },
+  {
+    "otel_isim": "Radisson Blu",
+    "sube_adi": "Radisson Blu Şişli",
+    "adres": "Büyükdere Cad. No: 19, Şişli, İstanbul",
+    "google_yildiz": 4.2,
+    "fotograf_linki": "https://www.google.com/search?q=Radisson+Blu+Şişli+hotel"
+  },
+  {
+    "otel_isim": "Lazzoni",
+    "sube_adi": "Lazzoni Hotel",
+    "adres": "Ortabayır Mah., Dereboyu Cd. No: 37, Beşiktaş, İstanbul",
+    "google_yildiz": 4.6,
+    "fotograf_linki": "https://www.google.com/search?q=Lazzoni+Hotel"
+  },
+  {
+    "otel_isim": "Richmond",
+    "sube_adi": "Richmond Nua Wellness Spa",
+    "adres": "Merkez Mah., Darıcadı Sokak No: 3, Kadıköy, İstanbul",
+    "google_yildiz": 4.1,
+    "fotograf_linki": "https://www.google.com/search?q=Richmond+Nua+Wellness+Spa"
+  },
+  {
+    "otel_isim": "Elite World",
+    "sube_adi": "Elite World Istanbul",
+    "adres": "Florya Mah., Yeşilköy Caddesi No: 47, Bakırköy, İstanbul",
+    "google_yildiz": 4.3,
+    "fotograf_linki": "https://www.google.com/search?q=Elite+World+Istanbul+hotel"
+  },
+  {
+    "otel_isim": "Radisson Blu",
+    "sube_adi": "Radisson Blu Taksim",
+    "adres": "Cihangir Mah., Sıraselviler Caddesi No: 8, Beyoğlu, İstanbul",
+    "google_yildiz": 4.0,
+    "fotograf_linki": "https://www.google.com/search?q=Radisson+Blu+Taksim+hotel"
+  },
+  {
+    "otel_isim": "Lazzoni",
+    "sube_adi": "Lazzoni Spa & Wellness",
+    "adres": "Ulus Mah., Ahmet Adnan Saygun Cd. No: 45, Beşiktaş, İstanbul",
+    "google_yildiz": 4.7,
+    "fotograf_linki": "https://www.google.com/search?q=Lazzoni+Spa+%26+Wellness"
+  },
+  {
+    "otel_isim": "Richmond",
+    "sube_adi": "Richmond İstanbul",
+    "adres": "İstinye Mah., Sakıp Sabancı Cd. No: 12, Sarıyer, İstanbul",
+    "google_yildiz": 4.2,
+    "fotograf_linki": "https://www.google.com/search?q=Richmond+İstanbul+hotel"
+  },
+  {
+    "otel_isim": "Elite World",
+    "sube_adi": "Elite World Asia",
+    "adres": "Ataköy Mah., Çobançeşme Yolu No: 14, Bakırköy, İstanbul",
+    "google_yildiz": 4.3,
+    "fotograf_linki": "https://www.google.com/search?q=Elite+World+Asia+hotel"
+  },
+  {
+    "otel_isim": "Radisson Blu",
+    "sube_adi": "Radisson Blu Ortaköy",
+    "adres": "Ortaköy Mah., Dereboyu Cd. No: 25, Beşiktaş, İstanbul",
+    "google_yildiz": 4.4,
+    "fotograf_linki": "https://www.google.com/search?q=Radisson+Blu+Ortaköy+hotel"
+  },
+  {
+    "otel_isim": "Lazzoni",
+    "sube_adi": "Lazzoni Court",
+    "adres": "Nispetiye Mah., Kuruçeşme Cd. No: 22, Beşiktaş, İstanbul",
+    "google_yildiz": 4.5,
+    "fotograf_linki": "https://www.google.com/search?q=Lazzoni+Court"
+  },
+  {
+    "otel_isim": "Richmond",
+    "sube_adi": "Richmond Kadıköy",
+    "adres": "Rasimpaşa Mah., Moda Cd. No: 15, Kadıköy, İstanbul",
+    "google_yildiz": 4.0,
+    "fotograf_linki": "https://www.google.com/search?q=Richmond+Kadıköy+hotel"
+  },
+  {
+    "otel_isim": "Elite World",
+    "sube_adi": "Elite World Florya",
+    "adres": "Florya Mah., Yeşilköy Caddesi No: 52, Bakırköy, İstanbul",
+    "google_yildiz": 4.2,
+    "fotograf_linki": "https://www.google.com/search?q=Elite+World+Florya+hotel"
+  },
+  {
+    "otel_isim": "Radisson Blu",
+    "sube_adi": "Radisson Blu Levent",
+    "adres": "Levent Mah., Büyükdere Cd. No: 22, Şişli, İstanbul",
+    "google_yildiz": 4.3,
+    "fotograf_linki": "https://www.google.com/search?q=Radisson+Blu+Levent+hotel"
+  },
+  {
+    "otel_isim": "Lazzoni",
+    "sube_adi": "Lazzoni Suites",
+    "adres": "Levazım Mah., Zeytinoğlu Cd. No: 33, Beşiktaş, İstanbul",
+    "google_yildiz": 4.6,
+    "fotograf_linki": "https://www.google.com/search?q=Lazzoni+Suites"
+  },
+  {
+    "otel_isim": "Richmond",
+    "sube_adi": "Richmond Ataşehir",
+    "adres": "Ataşehir Mah., Barbaros Cd. No: 10, Ataşehir, İstanbul",
+    "google_yildiz": 4.1,
+    "fotograf_linki": "https://www.google.com/search?q=Richmond+Ataşehir+hotel"
+  },
+  {
+    "otel_isim": "Elite World",
+    "sube_adi": "Elite World Luxury",
+    "adres": "Yeşilköy Mah., Havaalanı Caddesi No: 36, Bakırköy, İstanbul",
+    "google_yildiz": 4.5,
+    "fotograf_linki": "https://www.google.com/search?q=Elite+World+Luxury+hotel"
+  },
+  {
+    "otel_isim": "Radisson Blu",
+    "sube_adi": "Radisson Blu Maslak",
+    "adres": "Maslak Mah., Büyükdere Cd. No: 32, Sarıyer, İstanbul",
+    "google_yildiz": 4.4,
+    "fotograf_linki": "https://www.google.com/search?q=Radisson+Blu+Maslak+hotel"
+  },
+  {
+    "otel_isim": "Lazzoni",
+    "sube_adi": "Lazzoni Residence",
+    "adres": "Etiler Mah., Nispetiye Cd. No: 18, Beşiktaş, İstanbul",
+    "google_yildiz": 4.7,
+    "fotograf_linki": "https://www.google.com/search?q=Lazzoni+Residence"
+  },
+  {
+    "otel_isim": "Richmond",
+    "sube_adi": "Richmond Marmara",
+    "adres": "Kadıköy Mah., Moda Cd. No: 20, Kadıköy, İstanbul",
+    "google_yildiz": 4.0,
+    "fotograf_linki": "https://www.google.com/search?q=Richmond+Marmara+hotel"
+  },
+  {
+    "otel_isim": "Elite World",
+    "sube_adi": "Elite World Plaza",
+    "adres": "Ataköy Mah., Çobançeşme Yolu No: 16, Bakırköy, İstanbul",
+    "google_yildiz": 4.3,
+    "fotograf_linki": "https://www.google.com/search?q=Elite+World+Plaza+hotel"
+  },
+  {
+    "otel_isim": "Radisson Blu",
+    "sube_adi": "Radisson Blu Etiler",
+    "adres": "Etiler Mah., Nispetiye Cd. No: 20, Beşiktaş, İstanbul",
+    "google_yildiz": 4.5,
+    "fotograf_linki": "https://www.google.com/search?q=Radisson+Blu+Etiler+hotel"
+  },
+  {
+    "otel_isim": "Lazzoni",
+    "sube_adi": "Lazzoni Tower",
+    "adres": "Levent Mah., Büyükdere Cd. No: 40, Şişli, İstanbul",
+    "google_yildiz": 4.6,
+    "fotograf_linki": "https://www.google.com/search?q=Lazzoni+Tower"
+  },
+  {
+    "otel_isim": "Richmond",
+    "sube_adi": "Richmond Bosphorus",
+    "adres": "Bebek Mah., Cevdetpaşa Cd. No: 25, Beşiktaş, İstanbul",
+    "google_yildiz": 4.3,
+    "fotograf_linki": "https://www.google.com/search?q=Richmond+Bosphorus+hotel"
+  },
+  {
+    "otel_isim": "Elite World",
+    "sube_adi": "Elite World Garden",
+    "adres": "Florya Mah., Yeşilköy Caddesi No: 50, Bakırköy, İstanbul",
+    "google_yildiz": 4.2,
+    "fotograf_linki": "https://www.google.com/search?q=Elite+World+Garden+hotel"
+  },
+  {
+    "otel_isim": "Radisson Blu",
+    "sube_adi": "Radisson Blu Kadıköy",
+    "adres": "Kadıköy Mah., Moda Cd. No: 18, Kadıköy, İstanbul",
+    "google_yildiz": 4.1,
+    "fotograf_linki": "https://www.google.com/search?q=Radisson+Blu+Kadıköy+hotel"
+  },
+  {
+    "otel_isim": "Lazzoni",
+    "sube_adi": "Lazzoni Palace",
+    "adres": "Nispetiye Mah., Kuruçeşme Cd. No: 25, Beşiktaş, İstanbul",
+    "google_yildiz": 4.8,
+    "fotograf_linki": "https://www.google.com/search?q=Lazzoni+Palace"
+  },
+  {
+    "otel_isim": "Richmond",
+    "sube_adi": "Richmond İstinye",
+    "adres": "İstinye Mah., Sakıp Sabancı Cd. No: 15, Sarıyer, İstanbul",
+    "google_yildiz": 4.2,
+    "fotograf_linki": "https://www.google.com/search?q=Richmond+İstinye+hotel"
+  }
+];
+
+// Export the hotel data for use in other files
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { hotelData };
+} else {
+  // For browser use
+  window.hotelData = hotelData;
+}
